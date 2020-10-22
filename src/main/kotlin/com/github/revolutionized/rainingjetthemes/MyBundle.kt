3857c077fb6049dc46f403341120d5e62d6fc1d9
GitHub Action <action@github.com>
2020-10-22 06:09:37 +0000

    Template cleanup

--- src/main/kotlin/com/github/revolutionized/rainingjetthemes/MyBundle.kt
+++ src/main/kotlin/com/github/revolutionized/rainingjetthemes/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.revolutionized.rainingjetthemes
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/revolutionized/rainingjetthemes/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.revolutionized.rainingjetthemes.services
+
+import com.github.revolutionized.rainingjetthemes.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
